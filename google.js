894007885bb00f776cd0bb7a5242cccac32e22b4
Souhei Kodama <1419033@ed.tus.ac.jp>
2024-07-16 14:49:55 +0900

    refactor: Remove unused generationConfig

--- google.js
+++ google.js
@@ -3,13 +3,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const generationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 64,
-  responseMimeType: "text/plain",
-};
-
 export async function callGEMINI(prompt, message) {
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
