ea446c3cca9dcbe017d7ee185c9ef6acec6390c9
Souhei Kodama <1419033@ed.tus.ac.jp>
2024-07-16 14:19:10 +0900

    feat: English support

--- commitgen.js
+++ commitgen.js
@@ -3,36 +3,53 @@
 import "dotenv/config";
 import { callAzureChatGPT } from "./openai_azure.js";
 import { callChatGPT } from "./openai.js";
+import { callGEMINI } from "./google.js";
+
+const language = process.argv[2];
+const user_prompt = process.argv[3];
 
 const system_prompt =
-  "あなたはGit Commit コメント生成マシンです。入力されたdiff形式の差分コードに対して、72文字以内のサマリーコミットコメントを生成して下さい。可能な限り短い日本語の文章にして下さい。";
+  language === "ja"
+    ? "あなたはGit Commit コメント生成マシンです。入力されたdiff形式の差分コードに対して、72文字以内のサマリーコミットコメントを生成して下さい。可能な限り短い日本語の文章にして下さい。"
+    : "You are a Git Commit Comment Generator. Please generate a summary commit comment within 72 characters for the input diff-formatted code. Please make it as short as possible.";
 
-const user_prompt = process.argv[2];
+if (language) {
+  if (user_prompt) {
+    // Azure GPT-3
+    if (
+      process.env.AOAI_API_KEY &&
+      process.env.AOAI_ENDPOINT &&
+      process.env.AOAI_MODEL
+    ) {
+      const reply = await callAzureChatGPT(system_prompt, user_prompt);
+      console.log(reply);
 
-if (user_prompt) {
-  // Azure GPT-3
-  if (
-    process.env.AOAI_API_KEY &&
-    process.env.AOAI_ENDPOINT &&
-    process.env.AOAI_MODEL
-  ) {
-    const reply = await callAzureChatGPT(system_prompt, user_prompt);
-    console.log(reply);
+      // OpenAI GPT-3
+    } else if (process.env.OPENAI_API_KEY) {
+      const reply = await callChatGPT(system_prompt, user_prompt);
+      console.log(reply);
 
-    // OpenAI GPT-3
-  } else if (process.env.OPENAI_API_KEY) {
-    const reply = await callChatGPT(system_prompt, user_prompt);
-    console.log(reply);
+      // Google GEMINI
+    } else if (process.env.GOOGLE_API_KEY) {
+      const reply = await callGEMINI(system_prompt, user_prompt);
+      console.log(reply);
 
-    // Google GEMINI
-  } else if (process.env.GOOGLE_API_KEY) {
-    const reply = await callGEMINI(system_prompt, user_prompt);
-    console.log(reply);
-
-    // No API key
+      // No API key
+    } else {
+      const reply =
+        language === "ja"
+          ? "APIキーが設定されていないため、サマリが生成できませんでした"
+          : "The summary could not be generated because the API key is not set.";
+      console.log(reply);
+    }
   } else {
-    console.log("キーが設定されていないためサマリが生成できませんでした");
+    const reply =
+      language === "ja"
+        ? "入力がありません。コードを入力してください。"
+        : "There is no input. Please enter the code.";
+    console.log(reply);
   }
 } else {
-  console.log("変更はありません");
+  const reply = "No language specified. Please specify the language.";
+  console.log(reply);
 }
--- google.js
+++ google.js
@@ -1,28 +1,26 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const generationConfig = {
   temperature: 1,
-  presencePenalty: 0,
-  frequencyPenalty: 0,
-  topP: 1,
+  topP: 0.95,
+  topK: 64,
+  responseMimeType: "text/plain",
 };
 
 export async function callGEMINI(prompt, message) {
-  const system_prompt = { role: "system", content: prompt };
-  const user_prompt = { role: "user", content: message };
-
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstructions: system_prompt,
+    systemInstructions: prompt,
+    history: [],
   });
 
-  const completion = await model.startChat({
+  const completion = model.startChat({
     generationConfig,
   });
 
-  const result = await completion.sendMessage(user_prompt);
+  const result = await completion.sendMessage(message);
   return result.response.text();
 }
